4937c69800fd9b4c38f9dca469e5f7681245a3be
cgalvis <cgalvis@playvox.com>
2020-06-02 12:19:46 -0500

    Add python 2 challenges

---
+++ python2Chanllengs/unicodeSpecialCharacters.py
@@ -0,0 +1,10 @@
+if __name__ == "__main__":
+    unicode_one = u"ingl\u00E9s"
+    unicode_two = u"ingl\u00E9s"
+    number_test = 2
+    print(type(number_test))
+    print(type(unicode(number_test)))
+    """
+    print(unicode_one)
+    print(unicode_two == unicode_one)
+    """
